dec176ab6fd5d168ac3ffe2fcda5e3b9482348bf
KatieSil <katiesil@mail.ru>
2024-09-22 12:12:29 +0300

    Add files via upload

---
+++ module_2_hard.py
@@ -0,0 +1,13 @@
+n = int(input('Введите число от 3 до 20: '))
+pairs_numbers = []
+for i in range(1, 20) :
+    if i < n :
+        for j in range(i+1, 20) :
+            a = i + j
+            if n % a == 0 :
+                pairs_numbers.append(i)
+                pairs_numbers.append(j)
+
+result = pairs_numbers
+print('Нужный пароль, для введённого числа: ', *result, sep='')
+# print('Нужный пароль, для введённого числа: ', *result)
